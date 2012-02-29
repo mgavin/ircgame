44f78d41b271784a73874aaa5cac484f3050ca4f
Matthew <mgavin2@tigers.lsu.edu>
2012-02-29 07:43:53 -0600

    added main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//#include "IrcBot/ircbot.h"
+//#include "IrcBot/somthing.h"
+
+//using something;
+
+int main(int args, char* argv[])
+{/*
+  IrcBot* host = new IrcBot();
+
+  host->connect(server, ip, "IamTheHOST!");
+
+  while (host->running())
+  {
+      //do stuff
+      
+  }*/
+    const int* const a;
+  
+    return 0;
+}
